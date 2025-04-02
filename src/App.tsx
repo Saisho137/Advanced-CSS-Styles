df6bebe03033d6e58363800f36021a74a11b3b14
Santiago Betancur Duque <santib.2001@hotmail.com>
2025-04-01 19:53:39 -0500

    feat: add 3d logo rotation styles, vanilla version

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import "./App.scss";
 
 function App() {
-  return (
-    <>
-      <h1>Title</h1>
-      <h2>Description</h2>
-    </>
-  );
+	return (
+		<>
+			<h1>Title</h1>
+			<h2>Description</h2>
+			<Logo className="logo" />
+		</>
+	);
 }
 
 export default App;
+
+const Logo = ({ className }: { className: string }) => (
+	<svg
+		className={className}
+		width="349"
+		height="230"
+		viewBox="0 0 349 230"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg">
+		<path
+			d="M239.98 96.04L284.63 153.21L196.78 128.07L140.59 229.34L149.33 139.61L74.48 177.39L117.79 129.66L0 143.01L116.92 100.97L92.82 70.14L153.2 72.9L147.62 0L182.54 65.26L219.33 15.83L215.46 62.45L348.42 11.91L239.98 96.04Z"
+			fill="#009245"
+		/>
+	</svg>
+);
